859a80da39415bb662dbd4fa8e485c199a0e659d
Alexandre Neubert <aneubert@mobol.net>
2024-11-10 17:33:02 +0100

    adding code to handle props in vuejs 3

---
+++ vue-js/01_define-props/props/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Product available :price="1500" :name="product" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Product from './components/Product.vue'
+
+const product = 'Laptop'
+</script>
