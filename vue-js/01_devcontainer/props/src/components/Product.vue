b48c5efe5dbfe1d49dba86634e7d4cf8a57583b9
Alexandre Neubert <aneubert@mobol.net>
2024-11-24 22:00:25 +0100

    moved files to new directory

---
+++ vue-js/01_devcontainer/props/src/components/Product.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>{{ title }}</h3>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed } from 'vue'
+const props = defineProps<{
+  price: number
+  name: string
+  available: boolean
+}>()
+
+const title = computed(() => props.name.toUpperCase())
+</script>
