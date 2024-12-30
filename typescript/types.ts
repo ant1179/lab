114bc17c9940b3803e2307ef5af6f55e53165085
ant <aneubert@mobol.net>
2024-12-30 22:14:01 +0100

    auto commit

--- typescript/types.ts
+++ typescript/types.ts
@@ -2,3 +2,11 @@
 let firstname: string = "Alex";
 firstname = "Jeane";
 firstname = `James`;
+
+// numbers
+let age: number = 45;
+let realage: number = 45.3;
+
+//boolean
+let gender_male: boolean = true;
+let bold: boolean = false;
