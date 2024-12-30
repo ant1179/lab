5c4e055d55b66a39db0e17752232800cc6575737
ant <aneubert@mobol.net>
2024-12-30 23:02:01 +0100

    auto commit

--- typescript/types.ts
+++ typescript/types.ts
@@ -42,7 +42,7 @@ enum Role {
   READ_ONLY,
   READ_WRITE,
 }
-let user = { role: Role.ADMIN };
-if (user.role === Role.ADMIN) {
+let myRole: Role = Role.READ_ONLY;
+if ((myRole as Role) === Role.ADMIN) {
   console.log(`This is a test on an enum`);
 }
