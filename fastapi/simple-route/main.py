80f401fb6a11f551daa74cfa8bc27734dde67431
ant <aneubert@mobol.net>
2025-01-03 22:26:01 +0100

    auto commit

--- fastapi/simple-route/main.py
+++ fastapi/simple-route/main.py
@@ -1,8 +1,16 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy import create_engine, Column, Integer, String
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker, Session
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Database setup
+DATABASE_URL = &quot;sqlite:///./test.db&quot;
+engine = create_engine(DATABASE_URL);
 
 @app.get("/")
 async def root():
