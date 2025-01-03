dcefdece48213db99e3b1f3da8edb1bdf98a2691
ant <aneubert@mobol.net>
2025-01-03 22:38:01 +0100

    auto commit

--- fastapi/simple-route/main.py
+++ fastapi/simple-route/main.py
@@ -14,6 +14,19 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = sqlalchemy.orm.declarative_base()
 
+# Database model
+
+
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+
+
+# Create tables
+Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 async def root():
